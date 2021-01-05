d367f6ecd38dbd7095ce733b12d6353c359edde3
Dmytro Gamza <gamzadmytro@gmail.com>
2021-01-05 12:03:33 +0200

    Prime

--- newEndavors/asyncFuntions.js
+++ newEndavors/asyncFuntions.js
@@ -46,3 +46,41 @@ new Promise((resolve, reject) => {
 console.log('Script End');
 
 //26-11-2020
+
+//primeNumber
+const isPrime = num => {
+  for(let i = 2; i < num; i++)
+    if(num % i === 0) return false;
+  return num > 1;
+}
+console.log(isPrime(27));
+
+const isPrime2 = num => {
+    for(let i = 2, s = Math.sqrt(num); i <= s; i++)
+        if(num % i === 0) return false; 
+    return num > 1;
+}
+console.log(isPrime2(27));
+
+/*
+let num2 = prompt('Enter number to be checked for Prime');
+if (num2 <= 1) {
+     console.log('try again');
+} else if (num2 <= 3) {
+    console.log(`${num2} is Prime`);
+} else if (num2 % 2 == 0 || num2 % 3 == 0) {
+  console.log('try again');
+} else {
+   for (let i=5; i*i<=num2; i=i+6)
+   {
+     if (num2 % i == 0 || num2 % (i + 2) == 0) {
+           console.log('try again');
+      }else {
+    console.log(`${num2} is Prime`);
+    }
+  }
+}
+    
+*/
+  
+
